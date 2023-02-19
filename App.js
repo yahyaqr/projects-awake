0de837713cd45d62d977f55cee6931b83a32df88
yahyaqr <aqrom.yahya75@gmail.com>
2023-02-19 09:25:27 +0700

    last commit before branching

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Pressable, View, Text } from 'react-native';
 
 // Import Theme from React Native Paper
 import { Provider } from 'react-native-paper';
 import { theme } from './src/core/ReactNativePaperTheme';
 
 // Import Authentication from Firebase
-import firebase from '@react-native-firebase/app';
-import '@react-native-firebase/auth';
-import { FIREBASE_CONFIG } from './src/core/FirebaseConfig';
+// import auth from '@react-native-firebase/auth';
+// import firebase from '@react-native-firebase/app';
+// import { firebaseConfig } from './src/core/FirebaseConfig';
 
 // Import Navigators from React Navigation
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/AppNavigator';
+import UserNavigator from './src/navigator/UserNavigator';
+import AppNavigator from './src/navigator/AppNavigator';
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(FIREBASE_CONFIG);
+function App() {
+  const [user, setUser] = useState();
+
+  if (!user) {
+    return (
+      <Provider theme={theme}>
+        <NavigationContainer>
+          <UserNavigator />
+        </NavigationContainer>
+      </Provider>
+    );
+  }
+
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
 }
-
-const App = () => (
-  <Provider theme={theme}>
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-  </Provider>
-);
-
 export default App;
--- src/core/FirebaseConfig.js
+++ src/core/FirebaseConfig.js
@@ -1,4 +1,13 @@
-// Replace with your own firebase config!
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: 'AIzaSyAcBLTRY7XLFG1dqyA67xm-6kqrxCxRT1Y',
   authDomain: 'beforemidnight-awake-android.firebaseapp.com',
@@ -9,3 +18,7 @@ const firebaseConfig = {
   messagingSenderId: '923282210074',
   appId: '1:923282210074:web:906ce4d19f891a6c9c80a3',
 };
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
